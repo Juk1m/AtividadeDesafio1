6cff2d92ef254df969c2aaf8585e2363a3c2ffd6
Juk1m <149440044+Juk1m@users.noreply.github.com>
2023-10-31 22:00:23 -0300

    Initial commit

---
+++ AtividadeDesafio1/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace AtividadeDesafio1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.LbnPeso = new System.Windows.Forms.Label();
+            this.LbnAltura = new System.Windows.Forms.Label();
+            this.LbnImc = new System.Windows.Forms.Label();
+            this.TxtPeso = new System.Windows.Forms.TextBox();
+            this.TxtAltura = new System.Windows.Forms.TextBox();
+            this.TxtImc = new System.Windows.Forms.TextBox();
+            this.lbnResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(301, 148);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 0;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // LbnPeso
+            // 
+            this.LbnPeso.AutoSize = true;
+            this.LbnPeso.Location = new System.Drawing.Point(240, 77);
+            this.LbnPeso.Name = "LbnPeso";
+            this.LbnPeso.Size = new System.Drawing.Size(34, 13);
+            this.LbnPeso.TabIndex = 1;
+            this.LbnPeso.Text = "Peso:";
+            // 
+            // LbnAltura
+            // 
+            this.LbnAltura.AutoSize = true;
+            this.LbnAltura.Location = new System.Drawing.Point(240, 110);
+            this.LbnAltura.Name = "LbnAltura";
+            this.LbnAltura.Size = new System.Drawing.Size(34, 13);
+            this.LbnAltura.TabIndex = 2;
+            this.LbnAltura.Text = "Altura";
+            // 
+            // LbnImc
+            // 
+            this.LbnImc.AutoSize = true;
+            this.LbnImc.Location = new System.Drawing.Point(248, 186);
+            this.LbnImc.Name = "LbnImc";
+            this.LbnImc.Size = new System.Drawing.Size(26, 13);
+            this.LbnImc.TabIndex = 3;
+            this.LbnImc.Text = "IMC";
+            // 
+            // TxtPeso
+            // 
+            this.TxtPeso.Location = new System.Drawing.Point(290, 70);
+            this.TxtPeso.Name = "TxtPeso";
+            this.TxtPeso.Size = new System.Drawing.Size(100, 20);
+            this.TxtPeso.TabIndex = 4;
+            this.TxtPeso.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // TxtAltura
+            // 
+            this.TxtAltura.Location = new System.Drawing.Point(290, 107);
+            this.TxtAltura.Name = "TxtAltura";
+            this.TxtAltura.Size = new System.Drawing.Size(100, 20);
+            this.TxtAltura.TabIndex = 5;
+            this.TxtAltura.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // TxtImc
+            // 
+            this.TxtImc.AcceptsReturn = true;
+            this.TxtImc.Location = new System.Drawing.Point(290, 186);
+            this.TxtImc.Name = "TxtImc";
+            this.TxtImc.Size = new System.Drawing.Size(100, 20);
+            this.TxtImc.TabIndex = 6;
+            this.TxtImc.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // lbnResultado
+            // 
+            this.lbnResultado.AutoSize = true;
+            this.lbnResultado.Location = new System.Drawing.Point(295, 224);
+            this.lbnResultado.Name = "lbnResultado";
+            this.lbnResultado.Size = new System.Drawing.Size(0, 13);
+            this.lbnResultado.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbnResultado);
+            this.Controls.Add(this.TxtImc);
+            this.Controls.Add(this.TxtAltura);
+            this.Controls.Add(this.TxtPeso);
+            this.Controls.Add(this.LbnImc);
+            this.Controls.Add(this.LbnAltura);
+            this.Controls.Add(this.LbnPeso);
+            this.Controls.Add(this.btnCalcular);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label LbnPeso;
+        private System.Windows.Forms.Label LbnAltura;
+        private System.Windows.Forms.Label LbnImc;
+        private System.Windows.Forms.TextBox TxtPeso;
+        private System.Windows.Forms.TextBox TxtAltura;
+        private System.Windows.Forms.TextBox TxtImc;
+        private System.Windows.Forms.Label lbnResultado;
+    }
+}
+
---
+++ AtividadeDesafio1/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AtividadeDesafio1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtCalcular_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double p, a, i;
+
+            p = double.Parse(TxtPeso.Text);
+            a = double.Parse(TxtAltura.Text);
+            i = p / (a * a);
+
+            lbnResultado.Text = i.ToString();
+
+            if (i <= 18.5)
+            {
+                TxtImc.Text= i.ToString();
+                lbnResultado.Text = "Voce está abaixo do peso! ";
+            }
+            else if (i > 18.6 && i <= 24.9)
+            {
+                TxtImc.Text = i.ToString();
+
+                lbnResultado.Text = " Voce está com o peso ideal! ";
+            }
+            else if (i > 24.9 && i <= 29.9)
+            {
+                TxtImc.Text = i.ToString();
+
+                lbnResultado.Text = "Voce está levemente acima do peso! ";
+            }
+            else if (i > 30.0 && i <= 34.9)
+            {
+                TxtImc.Text = i.ToString();
+
+                lbnResultado.Text = " Voce está com grau 1 de Obesidade! ";
+            }
+            else if (i > 35.0 && i <= 39.9)
+            {
+
+                TxtImc.Text = i.ToString();
+
+                lbnResultado.Text = "Voce esta com grau 2 de Obesidade (Severa) ";
+            }
+            else
+            {
+                TxtImc.Text = i.ToString();
+
+                lbnResultado.Text = "Voce está com grau 3 de Obesidade (Morbida) ";
+            }
+
+            if (a <= 0 || p <= 0)
+            {
+                TxtPeso.Clear();
+                TxtAltura.Clear();
+                lbnResultado.Text = "";
+                TxtImc.Text = "";
+                MessageBox.Show("A sua altura ou Peso está Inválidos!");
+            }
+        }
+    }
+}
